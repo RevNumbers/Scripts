3edd73f9615438c83347681d8084285ecdd06ff8
Justin E. Goodell <jegoodell@gmail.com>
2011-08-08 13:10:01 -0400

    update to currently used patches

--- cyanogenmod/applycmpatchs.sh
+++ cyanogenmod/applycmpatchs.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-#Usb service change to use intents http://review.cyanogenmod.com/#change,6441
+#Overscroll Color (1/3) http://review.cyanogenmod.com/7148
+cd /home/cm4d2/packages/apps/CMParts
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_CMParts refs/changes/48/7148/9 && git cherry-pick FETCH_HEAD
+#Overscroll Color (2/3) http://review.cyanogenmod.com/7149
 cd /home/cm4d2/frameworks/base/
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/41/6441/1 && git cherry-pick FETCH_HEAD
-
-#add signal dBm option http://review.cyanogenmod.com/6904
-cd /home/cm4d2/frameworks/base/
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/04/6904/2 && git cherry-pick FETCH_HEAD
-
-#Separate Timeout for Screen and Security Lock http://review.cyanogenmod.com/6526
-cd /home/cm4d2/frameworks/base/
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/26/6526/1 && git cherry-pick FETCH_HEAD
-
-#add signal dBm option http://review.cyanogenmod.com/6903
-cd /home/cm4d2/packages/apps/CMParts/
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_CMParts refs/changes/03/6903/2 && git cherry-pick FETCH_HEAD
-
-#Separate Timeout for Screen and Security Lock http://review.cyanogenmod.com/6527
-cd /home/cm4d2/packages/apps/CMParts/
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_CMParts refs/changes/27/6527/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/49/7149/8 && git cherry-pick FETCH_HEAD
+#Overscroll Color (2/3) http://review.cyanogenmod.com/7166
+cd /home/cm4d2/vendor/tmobile/apps/ThemeChooser/
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/themes-platform-vendor-tmobile-apps-ThemeChooser refs/changes/66/7166/3 && git cherry-pick FETCH_HEAD
 
 #Fix hijack execution and logging (hijack logging flag must be uncommented in the BoardConfig.mk line 100) http://review.cyanogenmod.com/#change,7279
-cd /home/cm4d2/device/droid2/hijack
+cd /home/cm4d2/device/motorola/droid2/hijack
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_motorola_droid2 refs/changes/79/7279/1 && git cherry-pick FETCH_HEAD
+cd /home/cm4d2/device/motorola/droid2we/hijack
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_motorola_droid2 refs/changes/79/7279/1 && git cherry-pick FETCH_HEAD
+cd /home/cm4d2/device/motorola/shadow/hijack
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_motorola_droid2 refs/changes/79/7279/1 && git cherry-pick FETCH_HEAD
